dcf58d2baac0ea370982e3ceb9b801adeab90cbe
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2022-12-14 15:33:12 +0100

    2

--- index.php
+++ index.php
@@ -1,47 +1,3 @@
-<?php
-
-    $hotels = [
-
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -51,6 +7,67 @@
     <title>PHP Hotel</title>
 </head>
 <body>
-    
+
+<!-- Array di hotel. -->
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
+<h1>PHP Hotel</h1>
+
+<!-- Stampa dei dati -->
+
+<ul>
+    <?php foreach($hotels as $element) { ?>
+    <li>
+        <h3> <?php echo $element['name']; ?>  </h3>
+        <h3> <?php echo $element['description']; ?>  </h3>
+        <h3> <?php echo $element['parking']; ?>  </h3>
+        <h3> <?php echo $element['vote']; ?>  </h3>
+        <h3> <?php echo $element['distance_to_center']; ?>  </h3>
+    </li>
+    <?php } ?>
+</ul>
+
 </body>
 </html>
